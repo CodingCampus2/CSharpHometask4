63f206e611600e0500183d6a83234a774239575d
Tymur Antonov <tymur.antonov@gmail.com>
2020-03-31 14:09:36 +0300

    Added basic UnitTest for CI

---
+++ BasicTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using HomeworkTemplate;
+using CodingCampusCSharpHomework;
+
+namespace BasicTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false);//Task4.CheckSolver(Program.TaskSolver));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,15 +3,20 @@ using CodingCampusCSharpHomework;
 
 namespace HomeworkTemplate
 {
-    class Program
+    public class Program
     {
+        public static Func<Task4, char[,]> TaskSolver;
+
         static void Main(string[] args)
         {
-            Func<Task4, char[,]> TaskSolver = task =>
+            TaskSolver = task =>
             {
                 // Your solution goes here
                 // You can get all needed inputs from task.[Property]
                 // Good luck!
+                char[,] board = task.Board;
+
+                return board;
             };
 
             Task4.CheckSolver(TaskSolver);
